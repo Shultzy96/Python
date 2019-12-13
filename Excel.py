1e1013f1aa1841e7f9f118c2e0b6c402a7f25735
Shultzy96 <45430651+Shultzy96@users.noreply.github.com>
2019-12-12 19:24:22 -0600

    Code Modules
    All Modules Rely on "ImportEnvironment.py" as the Main import for any new module of python.

---
+++ Excel.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 12 17:27:51 2019 @author: Shultzy
+"""
+#|--------------- Imports ---------------|
+import sys
+sys.path.append("C:\Users\shult\Documents\PythonCode")
+import ImportEnvironment as Env
+#|--------------- Variables ---------------|
+
+#|--------------- Functions ---------------|
+
+def StartExcel():
+    xl = Env.c32.Dispatch("Excel.Application")
+    xl.Visible = True
+    return xl
+
+def OpenExcel():    
+    xl = Env.c32.Dispatch("Excel.Application")
+    xl.Visible = True
+    xl.Workbok.Open(FileName)
+    return xl
+    
+def GetExcel():
+    xl = Env.c32.Dispatch("Excel.Application")
+    return xl
+
+def ActivateSheet(wb, SheetName):
+    wb.Sheet(SheetName).activate
+    
+def SelectRange(wb, iCells):
+    wb.ActiveSheet.Range(iCells).Select
+
+def SetCellValue(wb, iCell, iValue):
+    wb.ActiveSheet.Range(iCell).Value = iValue
+
+def Copy(wb, iCells):
+    SelectRange(wb, iCells)
+    wb.ActiveSheet.Range(iCells).Copy
+
+def Paste(wb, iCell):
+    wb.ActiveSheet.Range(iCell).Paste
+
+def NewWorkbook(wbName):
+    xl = StartExcel()
+    xl.Add(wbName)
+    
+def AddSheet(ws, SheetName):
+    ws.Add(SheetName)
+    
+def SaveAs(wb, SaveName):
+    wb.SaveAs(SaveName)
+
+def DeleteSheet(wb, SheetName):
+    wb.Sheet(SheetName).Delete
+    
+def RenameWorkbook(wb, wbName):
+    wb.Name = wbName
+    
+def RenameSheet(ws, wsName):
+    ws.Name = wsName
+    
+def ExportPDF(wb, pdfName):
+    wb.ExportAsFixedFormat(0, pdfName, 0)
+    
+def ExportXPS(wb, xpsName):
+    wb.ExportAsFixedFormat(1, xpsName, 0)
+
+def GetPath(wb):
+    wbPath = wb.Path
+    return wbPath
---
+++ IExplorer.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 12 19:11:53 2019 @author: Shultzy
+"""
+#|--------------- Imports ---------------|
+import sys
+sys.path.append("C:\Users\shult\Documents\PythonCode")
+import ImportEnvironment as Env
+#|--------------- Variables ---------------|
+
+#|--------------- Functions ---------------|
+
+def StartIE():
+    ie = Env.c32.Dispatch("InternetExplorer.Application")
+    ie.Visible = True
+    return ie
+
+def OpenIE(URL):    
+    ie = Env.c32.Dispatch("InternetExplorer.Application")
+    ie.Visible = True
+    ie.Navigate(URL)
+    return ie
+    
+def GetIE():
+    ie = Env.c32.Dispatch("InternetExplorer.Application")
+    return ie
+
+def Navigate(ie, URL):
+    ie.Navigate(URL)
+
+def GoBack(ie):
+    ie.GoBack
+    
+def GoForward(ie):
+    ie.GoForward
+    
+def GoHome(ie):
+    ie.GoHome
+    
+def GoSearch(ie):
+    ie.GoSearch
+    
+def Refresh(ie):
+    ie.Refresh
+    
+def NewWindow(ie):
+    ie.NewWindow
+    
+    
